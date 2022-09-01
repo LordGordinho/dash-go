6ea406e14e8035efc98b4afb93b52713561e9112
Jorge Felipe <jfss1@ifal.edu.br>
2022-09-01 07:15:13 -0300

    add page user list

---
+++ src/pages/users/index.tsx
@@ -0,0 +1,71 @@
+import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Text, Th, Thead, Tr } from "@chakra-ui/react";
+import { RiAddLine, RiPencilLine } from "react-icons/ri";
+import { Header } from "../../components/Header";
+import { SideBar } from "../../components/SideBar";
+
+export default function UserList() {
+  return (
+    <Box>
+      <Header />
+
+      <Flex w="100%" flex="1" maxWidth={1480} mx="auto" px="6">
+        <SideBar />
+
+        <Box flex="1" borderRadius={8} bg="gray.800" p="8">
+          <Flex mb="8" justify="space-between" align="center">
+            <Heading size="lg">Usuários</Heading>
+
+            <Button 
+              as="a"
+              size="sm"
+              fontSize="sm"
+              colorScheme="pink"
+              leftIcon={<Icon as={RiAddLine} fontSize="20" />}
+            >
+              Criar Novo
+            </Button>
+          </Flex>
+
+          <Table colorScheme="whiteAlpha" >
+            <Thead >
+              <Tr>
+                <Th px="6" color="gray.300" width="8" >
+                  <Checkbox colorScheme="pink" />
+                </Th>
+                <Th>Usuários</Th>
+                <Th>Data de Cadastro</Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              <Tr>
+                <Td px="6">
+                  <Checkbox colorScheme="pink" />
+                </Td>
+                <Td>
+                  <Box>
+                    <Text fontWeight="bold">Jorge Felipe</Text>
+                    <Text fontSize="sm" color="gray.300">jorgefelipess08@gmail.com</Text>
+                  </Box>
+                </Td>
+                <Td>
+                  <Text>31 de agosto de 2022</Text>
+                </Td>
+                <Td>
+                <Button 
+                  as="a"
+                  size="sm"
+                  fontSize="sm"
+                  colorScheme="facebook"
+                  leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+                >
+                  Editar
+                </Button>
+                </Td>
+              </Tr>
+            </Tbody>
+          </Table>
+        </Box>
+      </Flex>
+    </Box>
+  )
+}
