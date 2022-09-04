cc9bbf6d935a8ce4bec099482fa965778f45e3d7
Jorge Felipe <jfss1@ifal.edu.br>
2022-09-04 07:55:37 -0300

    wip

--- src/components/Sidebar/NavLink.tsx
+++ src/components/Sidebar/NavLink.tsx
@@ -2,6 +2,7 @@ import { Icon, Link as ChakraLink, Text } from "@chakra-ui/react";
 import Link from "next/link";
 import { ElementType } from "react";
 import { RiDashboardLine } from "react-icons/ri";
+import { ActiveLink } from "../ActiveLink";
 
 interface NavLinkProps{
   title: string;
@@ -11,11 +12,11 @@ interface NavLinkProps{
 
 export function NavLink({ icon, title, href}: NavLinkProps){
   return (
-    <Link href={href} passHref>
+    <ActiveLink href={href} passHref>
       <ChakraLink display="flex" alignItems="center" >
         <Icon as={icon} fontSize="20" />
         <Text ml="4" fontWeight="medium" >{title}</Text>
       </ChakraLink>
-    </Link>
+    </ActiveLink>
   )
 }
